b8298a9122a677a9f55a65c811a83d09910f85de
leoD-E1 <leodangeli11@gmail.com>
2022-06-26 01:41:04 -0300

    [update] change port environment variable

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import "dotenv/config";
 
 import { server } from "./service/server";
 
-server.listen(process.env.SERVICE_PORT || 4000, () => {
-  console.log("%s listening on port %s", server.name, process.env.SERVICE_PORT);
+server.listen(process.env.PORT || 4000, () => {
+  console.log("%s listening on port %s", server.name, process.env.PORT);
 });
